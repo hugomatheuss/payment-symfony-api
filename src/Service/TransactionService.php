d6052abd5aceeddb9a65082efc7dc1396a1c51f0
hugo.matheus <hugo.matheus@voxtec.com.br>
2024-06-30 20:08:05 -0300

    criando validações para a transação.

---
+++ src/Service/TransactionService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Enum\UserTypeEnum;
+use App\Repository\UserRepository;
+
+class TransactionService
+{
+    public function __construct(private readonly UserRepository $userRepository)
+    {
+    }
+
+    public function validateTransaction($params)
+    {
+        $value = $params->value;
+        $payeeId = $params->payee;
+        $payerId = $params->payer;
+
+        /** @var User $user */
+        $user = $this->userRepository->findBy(['id' => $payeeId]);
+
+        if (!$this->validateUserCanTransfer($user)) {
+            throw new \Exception('User cannot do this transaction!');
+        }
+    }
+
+    protected function validateUserWallet()
+    {
+
+    }
+
+    protected function validateBalance()
+    {
+        //
+    }
+
+    protected function validateUserCanTransfer(User $user): bool
+    {
+        return $user->getUserType() === UserTypeEnum::COMMON;
+    }
+}
